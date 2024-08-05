c842d7636138d68a48fb61785ce1dec724bb7a7d
aniekmarkus <a.markus@erasmusmc.nl>
2024-08-05 19:46:37 +0200

    Add random sampling function

--- R/Sampling.R
+++ R/Sampling.R
@@ -33,8 +33,8 @@
 #' An object of class \code{sampleSettings}
 #' @export
 createSampleSettings <- function(type = 'none',
-                     numberOutcomestoNonOutcomes = 1,
-                     sampleSeed = sample(10000,1)){
+                                 numberOutcomestoNonOutcomes = 1,
+                                 sampleSeed = sample(10000,1)){
   
   checkIsClass(numberOutcomestoNonOutcomes, c('numeric','integer'))
   checkHigher(numberOutcomestoNonOutcomes,0)
@@ -47,7 +47,7 @@ createSampleSettings <- function(type = 'none',
   sampleSettings <- list(
     numberOutcomestoNonOutcomes = numberOutcomestoNonOutcomes,
     sampleSeed  = ifelse(type == 'none', 1, sampleSeed) # to make it the same for none
-    )
+  )
   
   if(type == 'none'){
     attr(sampleSettings, "fun") <- "sameData"
@@ -155,7 +155,7 @@ underSampleData <- function(trainData, sampleSettings){
   sampleTrainData$labels <- trainData$labels %>% dplyr::filter(.data$rowId %in% pplOfInterest)
   sampleTrainData$folds <- trainData$folds %>% dplyr::filter(.data$rowId %in% pplOfInterest)
   
-
+  
   
   sampleTrainData$covariateData <- Andromeda::andromeda()
   sampleTrainData$covariateData$covariateRef <- trainData$covariateData$covariateRef
@@ -255,3 +255,74 @@ overSampleData <- function(trainData, sampleSettings){
   
   return(sampleTrainData)
 }
+
+# Vignette https://ohdsi.github.io/PatientLevelPrediction/articles/AddingCustomSamples.html
+#' @export
+createRandomSampleSettings <- function(
+    n = 10000,
+    sampleSeed = sample(10000,1)
+){
+  
+  # add input checks
+  checkIsClass(n, c('numeric','integer'))
+  checkHigher(n,0)
+  checkIsClass(sampleSeed, c('numeric','integer'))
+  
+  # create list of inputs to implement function
+  sampleSettings <- list(
+    n = n,
+    sampleSeed  = sampleSeed
+  )
+  
+  # specify the function that will implement the sampling
+  attr(sampleSettings, "fun") <- "implementRandomSampleSettings"
+  
+  # make sure the object returned is of class "sampleSettings"
+  class(sampleSettings) <- "sampleSettings"
+  return(sampleSettings)
+  
+}
+
+# Vignette https://ohdsi.github.io/PatientLevelPrediction/articles/AddingCustomSamples.html
+implementRandomSampleSettings <- function(trainData, sampleSettings){
+  
+  n <- sampleSettings$n
+  sampleSeed <- sampleSettings$sampleSeed
+  
+  if(n > nrow(trainData$labels)){
+    stop('Sample n bigger than training population')
+  }
+  
+  # set the seed for the randomization
+  set.seed(sampleSeed)
+  
+  # now implement the code to do your desired sampling
+  
+  sampleRowIds <- sample(trainData$labels$rowId, n)
+  
+  sampleTrainData <- list()
+  
+  sampleTrainData$labels <- trainData$labels %>%
+    dplyr::filter(.data$rowId %in% sampleRowIds) %>%
+    dplyr::collect()
+  
+  sampleTrainData$folds <- trainData$folds %>%
+    dplyr::filter(.data$rowId %in% sampleRowIds) %>%
+    dplyr::collect()
+  
+  sampleTrainData$covariateData <- Andromeda::andromeda()
+  sampleTrainData$covariateData$covariateRef <-trainData$covariateData$covariateRef
+  sampleTrainData$covariateData$covariates <- trainData$covariateData$covariates %>% dplyr::filter(.data$rowId %in% sampleRowIds)
+  
+  #update metaData$populationSize
+  metaData <- attr(trainData$covariateData, 'metaData')
+  metaData$populationSize = n
+  attr(sampleTrainData$covariateData, 'metaData') <- metaData
+  
+  # make the cocvariateData the correct class
+  class(sampleTrainData$covariateData) <- 'CovariateData'
+  
+  # return the updated trainData
+  return(sampleTrainData)
+}
+
