61ae32a69bf4e40e66dd4d1beda6f0cc42dbbbb8
Aniek Markus <amarkus@mac.erasmusmc.nl>
2025-11-06 17:03:11 +0100

    Initial version convergence analysis and plot

--- R/FeatureImportance.R
+++ R/FeatureImportance.R
@@ -19,14 +19,13 @@
 
 #' Model Agnostic Feature Importance
 #' @export
-# TODO: add convergence
-computeFI <- function(plpResult, population, plpData, method="PFI", convergence = TRUE, repeats = 1,
-                      covariates = NULL, cores = NULL, log = NULL,
+computeFI <- function(plpResult, population, plpData, method="PFI", params = NULL,
+                      convergence = TRUE, covariates = NULL, cores = NULL, log = NULL,
                       logthreshold = "INFO") {
   if (method == "PFI_R") {
     fi <- pfi(plpResultEunomia, populationEunomia, plpDataEunomia)
   } else if (method %in% c("PFI", "SHAP")) {
-    fi <- fiSklearn(plpResultEunomia, populationEunomia, plpDataEunomia, method)
+    fi <- fiSklearn(plpResultEunomia, populationEunomia, plpDataEunomia, method, params, convergence)
   }
   
   return(fi)
@@ -34,9 +33,9 @@ computeFI <- function(plpResult, population, plpData, method="PFI", convergence 
 
 #' Model Agnostic Feature Importance
 #' @export
-fiSklearn <- function(plpResult, population, plpData, method="PFI", convergence = TRUE,
-                       covariates = NULL, cores = NULL, log = NULL,
-                       logthreshold = "INFO") {
+fiSklearn <- function(plpResult, population, plpData, method="PFI", params = NULL,
+                      convergence = TRUE, covariates = NULL, cores = NULL, log = NULL,
+                      logthreshold = "INFO") {
   if (!is.null(log)) {
     appender <- ParallelLogger::createFileAppender(
       layout = ParallelLogger::layoutParallel,
@@ -59,6 +58,15 @@ fiSklearn <- function(plpResult, population, plpData, method="PFI", convergence 
   }
   
   # TODO: check if input is sklearn model (or "pythonModule" exists) ?
+  # TODO: check correct input params per FI method
+  
+  params <- listCartesian(params)
+  
+  # if convergence = FALSE use first params
+  if (!convergence) {
+    params <- list(params[[1]])
+  }
+  # TODO: how to sort for convergence?
   
   # convert
   matrixObjects <- toSparseM(
@@ -89,21 +97,28 @@ fiSklearn <- function(plpResult, population, plpData, method="PFI", convergence 
   names <- matrixObjects$covariateMap
   names <- names[order(names$columnId),]
   
+  featureImportanceList <- list()
+  converged <- FALSE
+  p <- 0
+  metrics <- c()
+  
   if (method == "PFI") {
     reticulate::py_require("scikit-learn")
     sklearn <- reticulate::import("sklearn")
     
-    res <- sklearn$inspection$permutation_importance(estimator = model,
-                                                     X = X$toarray(),
-                                                     y = y$ravel(),
-                                                     scoring = "roc_auc",
-                                                     n_repeats = as.integer(1))
-    values <- res$importances_mean
-
-    varImp <- data.frame(
-      covariateId = covariates,
-      fi = values[names$covariateId %in% covariates]
-    )
+    while(!converged && p < length(params)) {
+      p <- p + 1 # next set of params
+      
+      res <- sklearn$inspection$permutation_importance(estimator = model,
+                                                       X = X$toarray(),
+                                                       y = y$ravel(),
+                                                       scoring = params[[p]]$scoring,
+                                                       n_repeats = as.integer(params[[p]]$n_repeats))
+      values <- res$importances_mean
+      
+      featureImportanceList[[p]] <- values[names$covariateId %in% covariates]
+      converged <- checkConvergence(featureImportanceList)
+    }
     
   } else if (method == "SHAP") {
     reticulate::py_require("shap")
@@ -111,25 +126,123 @@ fiSklearn <- function(plpResult, population, plpData, method="PFI", convergence 
     
     shap <- reticulate::import("shap")
     np <- reticulate::import("numpy")
-
-    explainer = shap$Explainer(model = model,
-                               masker = X$toarray(),
-                               link=shap$links$identity)
-
-    X_np <- np$array(as.matrix(matrixData))
-    shap_values = explainer(X_np)
     
-    feature_importance <- np$mean(np$abs(shap_values$values), axis=as.integer(0)) # compute average
-    
-    varImp <- data.frame(
-      covariateId = covariates,
-      fi = feature_importance[,1] # TODO: check columns
-    )
+    while(!converged && p < length(params)) {
+      p <- p + 1 # next set of params
+      
+      N <- nrow(matrixData)
+      background_size <- as.integer(ceiling(N * params[[p]]$background_sample))
+      X_background <- X[1L:background_size, ] # TODO: extend with random sample 
+      
+      explainer = shap$Explainer(model = model,
+                                 masker = X_background$toarray(),
+                                 link=shap$links$identity)
+      
+      X_np <- np$array(as.matrix(matrixData))
+      shap_values = explainer(X_np)
+      
+      feature_importance <- np$mean(np$abs(shap_values$values), axis=as.integer(0)) # compute average
+      
+      featureImportanceList[[p]] <- feature_importance[,1] # TODO: check columns
+      converged <- checkConvergence(featureImportanceList)
+    }
   }
-
+  
+  # create plot when converged or last params done
+  if (convergence) {
+    convergencePlot(featureImportanceList, params)
+  }
+  
+  varImp <- data.frame(
+    covariateId = covariates,
+    fi = featureImportanceList[[length(featureImportanceList)]]
+  )
+  
   return(varImp)
 }
 
+
+checkConvergence <- function(featureImportanceList, metric = "distance", stop = 0.005) {
+  
+  if (length(featureImportanceList) > 1) {
+    # compute pairwise similarity of most recent FIs
+    fi_prev <- featureImportanceList[[length(featureImportanceList)-1]]
+    fi_curr <- featureImportanceList[[length(featureImportanceList)]]
+    
+    value <- calculateConvergence(fi_prev, fi_curr, metric)
+    
+    # check stop criterion
+    if ((metric == "correlation" && value >= stop) ||
+        (metric %in% c("rmse", "max_diff", "distance") && value <= stop)) {
+      ParallelLogger::logInfo("Converged at params ", length(featureImportanceList), " (value = ", value, ")")
+      return(TRUE)
+    }
+  } 
+  
+  return(FALSE)
+}
+
+calculateConvergence <- function(fi_prev, fi_curr, metric = "distance") {
+  if (metric == "correlation") {
+    value <- cor(fi_prev, fi_curr)
+  } else if (metric == "rmse") {
+    value <- sqrt(mean((fi_prev - fi_curr)^2))
+  } else if (metric == "max_diff") {
+    value <- max(abs(fi_prev - fi_curr))
+  } else if (metric == "distance") {
+    value <- sqrt(sum((fi_prev-fi_curr)^2))
+  } else {
+    stop("Metric unknown")
+  }
+  return(as.numeric(value))
+}
+
+
+convergencePlot <- function(featureImportanceList, params, metric = "distance", stop = NULL) {
+  
+  if (length(featureImportanceList) > 1) {
+    
+    # Compute for all consecutive pairs
+    values <- numeric(length(featureImportanceList)-1)
+    for (i in 2:length(featureImportanceList)) {
+      v <- calculateConvergence(featureImportanceList[[i-1]], featureImportanceList[[i]], metric)
+      # print(v, digits=10)   # Print the raw value with higher precision
+      values[i-1] <- v
+    }
+    
+    # convert to df for plot
+    param_matrix <- do.call(rbind, lapply(params, function(x) unlist(x)))
+    df_plot <- as.data.frame(param_matrix)
+    df_plot <- df_plot[2:(length(values)+1),]
+    df_plot$value <- values
+    
+    # TODO: remove packages here
+    library(ggplot2)
+    library(dplyr)
+    
+    # 1 plot per setting
+    settings <- colnames(df_plot)[-length(colnames(df_plot))]
+    for (s in settings) {
+      df_plot_s <- df_plot %>%
+        group_by(!!sym(s)) %>%
+        summarise(avg_value = mean(value, na.rm = TRUE))
+      
+      plot <- ggplot2::ggplot(df_plot_s, aes(x = !!sym(s), y = avg_value)) +
+        geom_line(group=1) +
+        geom_point() +
+        labs(
+          x = paste0("Parameter ", s),
+          y = paste("FI ", metric),
+          title = "Convergence Analysis"
+        ) +
+        theme_minimal() + geom_hline(yintercept = stop, col = "red", linetype = 2)
+      
+      ggsave(paste0("plot_", s, "_", metric, ".png"), plot = plot)
+    }
+  } else {
+    ParallelLogger::logInfo("Need at least two sets of calculated FI.")
+  }
+}
 
 #' Permutation Feature Importance
 #'
@@ -176,7 +289,7 @@ pfi <- function(plpResult, population, plpData, repeats = 1,
       layout = ParallelLogger::layoutParallel,
       fileName = log
     )
-
+    
     logger <- ParallelLogger::createLogger(
       name = "PAR",
       threshold = logthreshold,
@@ -184,7 +297,6 @@ pfi <- function(plpResult, population, plpData, repeats = 1,
     )
     ParallelLogger::registerLogger(logger)
   }
-
   
   
   if (is.null(covariates)) { # TODO: why is this restricted to positive covariateImportance? 
@@ -195,7 +307,7 @@ pfi <- function(plpResult, population, plpData, repeats = 1,
   }
   
   # add code to format covariateData based on plpModel
-
+  
   if (!is.null(plpResult$model$preprocessing$featureEngineering)) {
     # do feature engineering/selection
     ParallelLogger::logInfo("Running FE in model")
@@ -209,7 +321,7 @@ pfi <- function(plpResult, population, plpData, repeats = 1,
   } else {
     ParallelLogger::logInfo("No FE in model")
   }
-
+  
   if (!is.null(plpResult$model$preprocessing$tidyCovariates)) {
     # do preprocessing
     ParallelLogger::logInfo("Applying data tidying done in model")
@@ -223,7 +335,7 @@ pfi <- function(plpResult, population, plpData, repeats = 1,
   } else {
     ParallelLogger::logInfo("No data tidying done in model")
   }
-
+  
   # apply prediction function
   pred <- do.call(
     attr(plpResult$model, "predictionFunction"),
@@ -233,13 +345,13 @@ pfi <- function(plpResult, population, plpData, repeats = 1,
       cohort = population
     )
   )
-
+  
   auc <- computeAuc(pred)
-
+  
   # do permulation and savePlpData to temp loc
   plpDataLocation <- file.path(tempdir(), paste0("data"))
   savePlpData(plpData, file = plpDataLocation)
-
+  
   if (is.null(cores)) {
     rlang::check_installed("parallel")
     ParallelLogger::logInfo(paste0("Number of cores not specified"))
@@ -260,9 +372,9 @@ pfi <- function(plpResult, population, plpData, repeats = 1,
   if (cores > 1) {
     cluster <- ParallelLogger::makeCluster(numberOfThreads = cores)
     ParallelLogger::clusterRequire(cluster, c("PatientLevelPrediction", "Andromeda"))
-
+    
     vpiSettings <- lapply(1:length(covariates), getVpiSettings)
-
+    
     aucP <- ParallelLogger::clusterApply(
       cluster = cluster,
       x = vpiSettings,
@@ -306,13 +418,13 @@ permutePerf <- function(settings) {
         return(NULL)
       }
     )
-
+    
     if (is.null(plpData)) {
       ParallelLogger::logInfo(paste0("plpData NULL for covariate: ", settings$covariateId))
       return(0)
     }
     ParallelLogger::logInfo(paste0("Calculating prediction for permutation of covariate: ", settings$covariateId))
-
+    
     # need to stop preprocessing and do it once...
     pred <- do.call(
       attr(settings$plpModel, "predictionFunction"),
@@ -322,7 +434,7 @@ permutePerf <- function(settings) {
         cohort = settings$population
       )
     )
-
+    
     auct <- computeAuc(pred)
     auc <- c(auc, auct)
   }
@@ -332,13 +444,13 @@ permutePerf <- function(settings) {
 
 permute <- function(plpDataLocation, cId, population) {
   plpData <- suppressWarnings(PatientLevelPrediction::loadPlpData(plpDataLocation))
-
+  
   # get analysisId
   aId <- plpData$covariateData$covariateRef %>%
     dplyr::filter(.data$covariateId == !!cId) %>%
     dplyr::select("analysisId") %>%
     dplyr::collect()
-
+  
   # if analysis id is not 3 (age group), 4 (race) or 5 (ethnicity)
   if (!aId$analysisId %in% c(3, 4, 5)) {
     # select covariateId data
@@ -346,11 +458,11 @@ permute <- function(plpDataLocation, cId, population) {
       dplyr::filter(.data$covariateId == !!cId) %>%
       dplyr::collect()
     nSamp <- length(coi$rowId)
-
+    
     # find a new random selection of people and give them the covariate and value
     newPlp <- sample(population$rowId, nSamp)
     newData <- dplyr::as_tibble(cbind(rowId = newPlp, coi[, -1]))
-
+    
     # swap old covariate data with new
     plpData$covariateData$covariates <- plpData$covariateData$covariates %>%
       dplyr::filter(.data$covariateId != !!cId) %>%
@@ -358,17 +470,17 @@ permute <- function(plpDataLocation, cId, population) {
     Andromeda::appendToTable(plpData$covariateData$covariates, newData)
   } else {
     # do some code for the connected variables... with more than 2 options
-
+    
     # get the ppl with covariates
     haveCidData <- plpData$covariateData$covariates %>%
       dplyr::filter(.data$covariateId == !!cId) %>%
       dplyr::collect()
     nSamp <- length(haveCidData$rowId)
-
+    
     # sample the pop to replace
     swapPlp <- sample(population$rowId, nSamp)
     haveCidDataSwapped <- dplyr::as_tibble(cbind(rowId = swapPlp, haveCidData[, -1]))
-
+    
     # find the swapped people to switch
     connectedCovs <- plpData$covariateData$covariateRef %>%
       dplyr::filter(.data$analysisId == !!aId$analysisId) %>%
@@ -379,10 +491,10 @@ permute <- function(plpDataLocation, cId, population) {
       dplyr::filter(.data$covariateId %in% !!connectedCovs$covariateId) %>%
       dplyr::filter(.data$rowId %in% swapPlp) %>%
       dplyr::collect()
-
+    
     swappedForCid <- dplyr::as_tibble(cbind(rowId = haveCidData$rowId[1:nrow(plpToSwap)], plpToSwap[, -1]))
-
-
+    
+    
     # swap old covariate data with new
     plpData$covariateData$covariates <- plpData$covariateData$covariates %>%
       dplyr::filter(.data$covariateId != !!cId) %>%
@@ -393,6 +505,6 @@ permute <- function(plpDataLocation, cId, population) {
       rbind(haveCidDataSwapped, swappedForCid)
     )
   }
-
+  
   return(plpData)
 }
